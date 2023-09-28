fd88d1a289b9c91889ae8447116a0b72c48e4fb4
GD-Asoka <51798666+GD-Asoka@users.noreply.github.com>
2023-09-28 15:43:51 -0700

    Fixed Anims

---
+++ SuperDAB/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager AM_instance;
+    public AudioSource audioSource;
+
+    [SerializeField] private AudioClip _defaultBGM;
+
+    public enum BGMState
+    {
+        Default,
+        RunningOutOfTime,
+        DoingWell,
+        Calm
+    }
+
+    private BGMState _bgmState = BGMState.Default;
+
+    private void Awake()
+    {
+        if (AM_instance)
+        {
+            Destroy(AM_instance.gameObject);
+        }
+        else
+        {
+            AM_instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        UpdateBGM(BGMState.Default);
+    }
+
+    public void UpdateBGM(BGMState state)
+    {
+        audioSource.loop = true;
+        switch (state)
+        {
+            case BGMState.Default:
+                audioSource.clip = _defaultBGM;
+                break;
+            default:
+                audioSource.clip = _defaultBGM;
+                break;
+        }
+        audioSource.Play();
+    }
+}
